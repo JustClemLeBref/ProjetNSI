dde05f605c2f1cedf59c68447229baf6cf151bf2
imtrust <95450876+imtrust@users.noreply.github.com>
2021-12-10 14:22:15 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,22 +1,40 @@
-import pygame # utilise le module pygame
-import keyboard  # utilise le module keyboard
+#module import
+import pygame 
+import keyboard  
+import sys
 
-pygame.init() #creer une fenetre 
-screen = pygame.display.set_mode((720,500)) #dimension de la fentre
-pygame.display.flip() #flip la fenectre
-pygame.display.set_caption("Kirby") #donne un nom en haut de la fenectre
-actif = True #donne la valeur "true" a actif
+#creer une fenetre 
+pygame.init() 
 
+#dimension de la fenetre
+screen_width = 1440
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width, screen_height)) 
+
+#ajoute en plus
+pygame.display.flip() 
+clock = pygame.time.Clock()
+
+#titre et background et icon
+pygame.display.set_caption("Kirby") 
+icon = pygame.image.load(r'U:\1ere\NSI\projet\icon.jpg')
+pygame.display.set_icon(icon)
+background = pygame.image.load(r"U:\1ere\NSI\projet\bg.png")
+
+#variable
+actif = True 
+
+#la loop
 while actif:  # creer _une loop
-
+    screen.blit(background,(0,0))
+    pygame.display.update()
     for event in pygame.event.get(): #prends chaque evenement de pygame 
         if event.type == pygame.QUIT: #je compars levenement pris
             pygame.quit()
-            sys.exit()
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # si la touche 'q' est appuier 
-            print('You Pressed A Key!') 
+        if keyboard.is_pressed('ESC'):  # si la touche 'q' est appuier 
+            print('You Pressed ECHAP Key!') 
             pygame.quit()
-            sys.exit()
     except:
         break  # if user pressed a key other than the given key the loop will break
+    
