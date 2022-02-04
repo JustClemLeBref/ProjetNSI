512e384ea9205d505d5b325b580e6d00b2793530
Zwanzig01 <95105497+Zwanzig01@users.noreply.github.com>
2022-02-04 14:48:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,83 @@
+
 #importation des modules nécessaires
 import pygame
 import keyboard
 import random
 
- 
-#création de la fenetre
-pygame.init()
-
 #dimension de la fenetre
 screen_width = 1440
 screen_height = 1000
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-#titre, background et icon
-pygame.display.set_caption("BLOOBEY")
-background = pygame.image.load('GRAPHISME/Background_Sized.png')
-icon = pygame.image.load('GRAPHISME/bloobey-logo.png')
-pygame.display.set_icon(icon)
-
-#Image du GameOver
-GameOver=pygame.image.load('GRAPHISME/GAMEOVER.jpg')
-GameOver = pygame.transform.scale(GameOver, (1000, 1000))
-
-white = (255, 255, 255)
-red = (255, 0, 0)
-black = (0, 0, 0)
 
 #on ajoute en plus
 pygame.display.flip()
 clock = pygame.time.Clock()
 
 
+white = (255, 255, 255)
+red = (255, 0, 0)
+black = (0, 0, 0)
+
+
+
 #classe du joueur, ses stats
 class game_character(pygame.sprite.Sprite):
-    def __init__(self,image):
+    def __init__(self,image,size):
         super().__init__()
         self.description = "default"
-        self.image = pygame.image.load(image)
-        self.size = (150,150)
-        self.transform = pygame.transform.scale(self.image, (self.size[0], self.size[1]))
+        self.image = pygame.transform.scale(image, (size[0], size[1]))
         self.x = 300
         self.y = 640
         self.rect = self.image.get_rect()
-        isJump = False
+        # Set speed vector of player
+        self.change_x = 0
+        self.change_y = 0 
+        # List of sprites we can bump against
+        self.level = None
 
     #création de la gravité
     def update(self):
         self.calc_grav()
         self.rect.topleft = self.x, self.y
-
+        
     def calc_grav(self):
-        if self.y <= screen_height - 150 and self.y >= 0:
-            if not(isJump):
-                if jumpCount>= -10:
-                    self.y -= (jumpCount * abs(jumpCount)) * 0,5
-                    jumpCount -= 1
-                else:
-                    jumpCount = 10
-                    isJump=False
-                    
-            
-    #variable pour le saut du joueur
-    def jump(self):
-        if not(isJump):
-            if keyboard.is_pressed('z'):
-                isJump = True
+        """ Calculate effect of gravity. """
+        if self.change_y == 0:
+            self.change_y = 1
         else:
-            if jumpCount>= 0:
-                self.y -= (jumpCount * abs(jumpCount)) * 0,5
-                jumpCount -= 1
-            else:
-                jumpCount = 10
-                isJump=False
+            self.change_y += .35
+ 
+        # See if we are on the ground.
+        if self.rect.y >= screen_height - self.rect.height and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.y = screen_height - self.rect.height
+ 
+    def jump(self):
+        """ Called when user hits 'jump' button. """
+ 
+        # move down a bit and see if there is a platform below us.
+        # Move down 2 pixels because it doesn't work well if we only move down
+        # 1 when working with a platform moving down.
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2
+ 
+        # If it is ok to jump, set our speed upwards
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = -10
+ 
+    # Player-controlled movement:
+    def go_left(self):
+        """ Called when the user hits the left arrow. """
+        self.change_x = -6
+ 
+    def go_right(self):
+        """ Called when the user hits the right arrow. """
+        self.change_x = 6
+ 
+    def stop(self):
+        """ Called when the user lets off the keyboard. """
+        self.change_x = 0
+ 
             
             
 #classe pour les ennemies, ses stats
@@ -79,21 +85,27 @@ class Ennemie(pygame.sprite.Sprite):
     def __init__(self,image):
         super().__init__()
         self.description = "default"
-        self.image = pygame.image.load(image)
         self.size = (150,150)
-        self.transform = pygame.transform.scale(self.image, (self.size[0], self.size[1]))
+        self.image = pygame.transform.scale(pygame.image.load(image), (self.size[0], self.size[1]))
         self.x = 300
         self.y = 640
         self.rect = self.image.get_rect()
-        self.isJump = False
-        self.jumpCount = 10
-        self.vel = 100
-        self.speed = 20
     
     def update(self):
-
         self.rect.topleft = self.x, self.y
-
+    def deplacement(self):
+        if Position==0:
+            if Ennemie_obj.x != 1000:
+                Ennemie_obj.x += 10
+                Ennemie_obj.image=Ennemie_copy
+            else:
+                Position = 1
+        if Position == 1:
+            if Ennemie_obj.x!=-10:
+                Ennemie_obj.x -= 10
+                Ennemie_obj.image = Ennemie_with_flip
+            else:
+                Position = 0
 class Platform(pygame.sprite.Sprite):
     #La Platforme où Bloobey saute 
  
@@ -129,11 +141,40 @@ class BUTTON(pygame.sprite.Sprite):
     def update(self):
         self.rect.topleft = self.x, self.y
         
+class Level(object):
+    """ This is a generic super-class used to define a level.
+        Create a child class for each level with level-specific
+        info. """
+ 
+    def __init__(self, player):
+        """ Constructor. Pass in a handle to player. Needed for when moving platforms
+            collide with the player. """
+        self.platform_list = pygame.sprite.Group()
+        self.enemy_list = pygame.sprite.Group()
+        self.player = player
+         
+        # Background image
+        self.background = None
+ 
+    # Update everythign on this level
+    def update(self):
+        """ Update everything in this level."""
+        self.platform_list.update()
+        self.enemy_list.update()
+ 
+    def draw(self):
+        """ Draw everything on this level. """
+ 
+ 
+        # Draw all the sprite lists that we have
+        self.platform_list.draw(screen)
+        self.enemy_list.draw(screen)
+ 
 # Create platforms for the level
-class Level():
+class Level_1(Level):
     """ Definition for level 1. """
  
-    def __init__(self):
+    def __init__(self,game_character):
         """ Create level 1. """
  
         self.platform_list = pygame.sprite.Group()
@@ -142,6 +183,7 @@ class Level():
                  [210, 70, 200, 400],
                  [210, 70, 600, 300],
                  [210, 70, 200, 200],
+                 [400, 500, 200, 200],
                  ]
  
         # Go through the array above and add platforms
@@ -168,165 +210,158 @@ def restart():
     vel = 650
     y = 50
 
-#Bloobey, apparition sur l'écran      
-SLIME_obj_image = 'GRAPHISME/bloobey-logo.png'
-SLIME_obj = game_character(SLIME_obj_image)
-SLIME_obj.description = "Slimey"
-SLIME_obj.size = (200,200)
-SLIME_obj.image = SLIME_obj.transform
-coordone_SLIME_obj=(400,750)
-SLIME_obj.x = coordone_SLIME_obj[0]
-SLIME_obj.y = coordone_SLIME_obj[1]
-
-#apparition de l'ennemi sur l'écran
-Ennemie_obj_image = 'GRAPHISME/monstre_test.png'
-Ennemie_obj = Ennemie(Ennemie_obj_image)
-Ennemie_obj.description = "Enemy"
-Ennemie_obj.size = (150,150)
-Ennemie_obj.image = SLIME_obj.transform
-coordone_Ennemie_obj=(300,500)
-Ennemie_obj.x = coordone_Ennemie_obj[0]
-Ennemie_obj.y = coordone_Ennemie_obj[1]
-
-
-all_sprites = pygame.sprite.Group(Ennemie_obj, SLIME_obj)
-SLIMES = pygame.sprite.Group(SLIME_obj)
-
-#boutons Yes et No et leurs valeurs
-image_YES = 'GRAPHISME/YES.png'
-image_NO = 'GRAPHISME/NO.png'
-
-YES = BUTTON(image_YES)
-YES.image = YES.transform
-coordone_YES=(400,650)
-YES.x = coordone_YES[0]
-YES.y = coordone_YES[1]
-YES.endresult = 0
-
-NO = BUTTON(image_NO)
-NO.image = NO.transform
-NO.x = YES.x + 450
-NO.y = YES.y
-NO.endresult = 2
-
-BUTTONS = pygame.sprite.Group(YES, NO)
-
-SLIME_copy = SLIME_obj.image
-SLIME_with_flip = pygame.transform.flip(SLIME_copy, True, False)
-
-Ennemie_copy = pygame.image.load('GRAPHISME/monstre_test(1).png')
-Ennemie_with_flip = pygame.transform.flip(Ennemie_copy, True, False)
-   
-isJump = False
-jumpCount = 10
-vel = 100
-y = 50
-Position=0
-color=0
-touched = 0
-ancien=SLIME_obj.x
-hearts=1
-
-active = True
+def GameOver_Scene():
+    #Image du GameOver
+    GameOver=pygame.image.load('GRAPHISME/GAMEOVER.jpg')
+    GameOver = pygame.transform.scale(GameOver, (1000, 1000))
+    
+    #boutons Yes et No et leurs valeurs
+    image_YES = 'GRAPHISME/YES.png'
+    image_NO = 'GRAPHISME/NO.png'
+    
+    YES = BUTTON(image_YES)
+    YES.image = YES.transform
+    coordone_YES=(400,650)
+    YES.x = coordone_YES[0]
+    YES.y = coordone_YES[1]
+    YES.endresult = 0
+    
+    NO = BUTTON(image_NO)
+    NO.image = NO.transform
+    NO.x = YES.x + 450
+    NO.y = YES.y
+    NO.endresult = 2
+    
+    BUTTONS = pygame.sprite.Group(YES, NO)
+    
+    display_surface.blit(GameOver,(200,100))
+    
+    BUTTONS.update()
+    
+    BUTTONS.draw(display_surface)
+    
+    if YES.click(event_list):
+        hearts = 1
+        color=0
+        restart()
+    #si on appuie sur 'No', on quitte le jeu    
+    if NO.click(event_list):
+        active = False
 
 
+def main():
+    """ Main Program """
+    pygame.init()
+    
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    
+    #titre, background et icon
+    pygame.display.set_caption("BLOOBEY")
+    background = pygame.image.load('GRAPHISME/Background_Sized.png')
+    icon = pygame.image.load('GRAPHISME/bloobey-logo.png')
+    pygame.display.set_icon(icon)
+    
+    #Bloobey, apparition sur l'écran      
+    SLIME_obj_image = pygame.image.load('GRAPHISME/bloobey-logo.png')
+    SLIME_obj_size = (200,200)
+    SLIME_obj = game_character(SLIME_obj_image,SLIME_obj_size)
+    coordone_SLIME_obj=(400,750)
+    SLIME_obj.x = coordone_SLIME_obj[0]
+    SLIME_obj.y = coordone_SLIME_obj[1]
+    
+    #apparition de l'ennemi sur l'écran
+    Ennemie_obj_image = 'GRAPHISME/monstre_test.png'
+    Ennemie_obj = Ennemie(Ennemie_obj_image)
+    Ennemie_obj.description = "Enemy"
+    Ennemie_obj.size = (150,150)
+    coordone_Ennemie_obj=(300,500)
+    Ennemie_obj.x = coordone_Ennemie_obj[0]
+    Ennemie_obj.y = coordone_Ennemie_obj[1]
+    
+    SLIME_copy = SLIME_obj.image
+    SLIME_with_flip = pygame.transform.flip(SLIME_copy, True, False)
+    
+    Ennemie_copy = pygame.image.load('GRAPHISME/monstre_test(1).png')
+    Ennemie_with_flip = pygame.transform.flip(Ennemie_copy, True, False)
 
-while active:
     # Create all the levels
     level_list = []
-    level_list.append( Level() )
+    level_list.append( Level_1(SLIME_obj) )
     
-    # Set the current level
-    current_level_no = 0
-    current_level = level_list[current_level_no]
- 
-    if SLIME_obj.x != ancien:
-        print(SLIME_obj.x)
-        print(SLIME_obj.y)
-        ancien=SLIME_obj.x
-
-    event_list = pygame.event.get()
-
-    for event in event_list:
-        if event.type == pygame.QUIT:
-            active = False
-            
-
-    try:  # try préviens le code si une autre touche que celle donné est appuyé par l'utilisateur
-        if keyboard.is_pressed('ESC'): # si la touche 'échappe' est pressé
-            print('You Pressed ECHAP Key!')
-            active = False
-    except:
-        break  # si l'utilisateur appuie sur une autre touche, la boucle s'arrete
+    isJump = False
+    jumpCount = 10
+    vel = 100
+    y = 50
+    ancien=SLIME_obj.x
+    hearts=1 
     
-    #recherche de collision
-
-    all_sprites.update()
-
-    collided_bananas = pygame.sprite.spritecollide(Ennemie_obj, SLIMES, False)
-    for collided_banana in collided_bananas:
-        if hearts == 3:
-            pygame.time.delay(10)
-            color=1
-        else:
-            restart()
-            hearts+=1
-        #chargement des personages
+    active = True
     
-    try:
-        #si la touche 'd' est appuyer
-        if keyboard.is_pressed('d'):
-            if SLIME_obj.x != 1000:
-                SLIME_obj.x=SLIME_obj.x+10
-            SLIME_obj.image=SLIME_with_flip
-        # si la touche 'd' est appuyer
-        if keyboard.is_pressed('q'):
-            if SLIME_obj.x!=-10:
-                SLIME_obj.x=SLIME_obj.x-10
-            SLIME_obj.image=SLIME_copy
-        SLIME_obj.jump()
-    except:
-        break  # si l'utilisateur appuie sur une autre touche, la boucle s'arrete
+    all_sprites = pygame.sprite.Group(Ennemie_obj, SLIME_obj)
+    SLIMES = pygame.sprite.Group(SLIME_obj)
 
-    #boucle des mouvements du monstre
-
-    if Position==0:
-        if Ennemie_obj.x != 1000:
-            Ennemie_obj.x += 10
-            Ennemie_obj.image=Ennemie_copy
-        else:
-            Position = 1
-    if Position == 1:
-        if Ennemie_obj.x!=-10:
-            Ennemie_obj.x -= 10
-            Ennemie_obj.image = Ennemie_with_flip
-        else:
-            Position = 0
     
-    #changement de scene
-    display_surface = pygame.display.set_mode((screen_width, screen_height))
+    while active:
 
-    if color == 1:
-        display_surface.blit(GameOver,(200,100))
-        BUTTONS.update()
-        BUTTONS.draw(display_surface)
-        YES.click(event_list)
-        if YES.click(event_list):
-            hearts = 1
-            color=0
-            restart()
-        #si on appuie sur 'No', on quitte le jeu    
-        if NO.click(event_list):
-            active = False
-    if color == 2:
-        active = False
         
-    if color == 0:
+        # Set the current level
+        current_level_no = 0
+        current_level = level_list[current_level_no]
+        
+        if SLIME_obj.x != ancien:
+            print(SLIME_obj.x)
+            print(SLIME_obj.y)
+        ancien=SLIME_obj.x
+    
+        event_list = pygame.event.get()
+            
+        keys = pygame.key.get_pressed() 
+          
+        for event in event_list:
+            if event_list == pygame.QUIT:
+                active = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    print('You Pressed ECHAP Key!')
+                    active = False
+                if event.key == pygame.K_RIGHT:
+                    if SLIME_obj.x != 1000:
+                        SLIME_obj.x=SLIME_obj.x+10
+                    SLIME_obj.image=SLIME_with_flip
+                if event.key == pygame.K_LEFT:
+                    if SLIME_obj.x!=-10:
+                        SLIME_obj.x=SLIME_obj.x-10
+                    SLIME_obj.image=SLIME_copy
+                if event.key == pygame.K_UP:
+                    SLIME_obj.jump()
+                if event.type == pygame.QUIT:
+                    active = False
+
+        
+        #recherche de collision
+    
+        all_sprites.update()
+    
+        collided_bananas = pygame.sprite.spritecollide(Ennemie_obj, SLIMES, False)
+        for collided_banana in collided_bananas:
+            if hearts == 3:
+                pygame.time.delay(10)
+                color=1
+            else:
+                restart()
+                hearts+=1
+    
+        
+        #changement de scene
+        display_surface = pygame.display.set_mode((screen_width, screen_height))
         display_surface.blit(background,(0,0))
         all_sprites.draw(screen)
         current_level.draw(screen)
-        
-    pygame.display.update()
-    clock.tick(30)
-pygame.quit()
-#fin du code et sortie de la fenêtre
+        pygame.display.update()
+        clock.tick(30)
+    #fin du code et sortie de la fenêtre
+    pygame.quit()
+
+
+main()
+
