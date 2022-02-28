26716489b72ac7ed366a5aa77b836e5898ec798e
imtrust <95450876+imtrust@users.noreply.github.com>
2022-02-28 11:49:39 +0100

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,7 +1,7 @@
 import pygame
+
 import random
-
-
+from pygame import mixer
 
 class BUTTON(pygame.sprite.Sprite):
     def __init__(self,image,size):
@@ -26,12 +26,9 @@ class BUTTON(pygame.sprite.Sprite):
         self.rect.topleft = self.x, self.y
 
 
-
-
-
-
 def mainmenu():
     pygame.init() 
+    
     
     white = (255, 255, 255) 
     
@@ -42,7 +39,12 @@ def mainmenu():
     
     pygame.display.set_caption('Image')   
     image =  pygame.image.load('.\\main_screen_images\\Background_Sized.png') 
-
+    
+    #def de la musique
+    pygame.mixer.init()
+    pygame.mixer.music.load('.\\main_screen_images\\musique_fort_boyard.ogg')
+    pygame.mixer.music.play()
+    
     #def position et importation bouton play
     image_play = '.\\main_screen_images\\Play_button.png'
     play = BUTTON(image_play,(350,250))
@@ -50,7 +52,6 @@ def mainmenu():
     play.x = coordone_play[0]
     play.y = coordone_play[1]
     play.endresult = 0
-    
     
     #def position et importation monstre 
     image_monster = '.\\main_screen_images\\Monster.png'
@@ -86,8 +87,20 @@ def mainmenu():
     Isetting.y = coordone_Isetting[1]
     Isetting.endresult = 0
     
+    #def position et importation  icone info
+    image_icone_info = '.\\main_screen_images\\icone_info.png'
+    Iinfo = BUTTON(image_icone_info,(100,100))
+    coordone_Iinfo=(1290,0)
+    Iinfo.x = coordone_Iinfo[0]
+    Iinfo.y = coordone_Iinfo[1]
+    Iinfo.endresult = 0
     
-    BUTTONS = pygame.sprite.Group(play,monster,bloobey,Tbloobey,Isetting)
+    
+    
+    
+    
+    BUTTONS = pygame.sprite.Group(play,monster,bloobey,Tbloobey,Isetting,Iinfo)
+    Imenu=False
     menu= False
     continuer = True
     while continuer :
@@ -95,28 +108,31 @@ def mainmenu():
         event_list=pygame.event.get()
         display_surface.fill(white) 
         display_surface.blit(image, (0, 0))
-    
-    
-    
-                
         
         BUTTONS.update()
         BUTTONS.draw(display_surface)
-        
+        #paramètre du clickage settings
         if Isetting.click(event_list):
             menu=not(menu)
         if menu:
             menu_image = pygame.image.load('.\\main_screen_images\\setting2.png')
             display_surface.blit(menu_image, (525, 250))
+        
+        #paramètre du clickage info
+        if Iinfo.click(event_list):
+            Imenu=not(Imenu)
+        if Imenu:
+            Imenu_image = pygame.image.load('.\\main_screen_images\\image_info.png')
+            display_surface.blit(Imenu_image, (175, 250))
     
-             
-                
-                
+        
+        
+        
+         #paramètre du clickage play       
         if play.click(event_list):
             continuer=False
             pygame.quit() 
     
-            
         for event in event_list : 
     
             if event.type == pygame.QUIT :
